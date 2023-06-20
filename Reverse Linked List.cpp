049fab87a71b4b7c8f895422868e06a094ea5d04
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2023-06-20 10:56:44 +0530

    Create Reverse Linked List.cpp

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,31 @@
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+
+{
+
+    // Write your code here
+
+    if(head==NULL) return NULL;
+
+    LinkedListNode<int>*p=NULL,*c=head,*n=head->next;
+
+    while(c!=NULL)
+
+    {
+
+        c->next=p;
+
+        p=c;
+
+        c=n;
+
+        if(n!=NULL)n=n->next;
+
+ 
+
+    }
+
+    return p;
+
+    
+
+}
