376c91a910f25a22055d0cf91e126841abc556f0
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2024-06-29 10:31:10 +0530

    Update Cycle Detection in a Singly Linked List.cpp

--- Cycle Detection in a Singly Linked List.cpp
+++ Cycle Detection in a Singly Linked List.cpp
@@ -27,28 +27,7 @@
 
 *****************************************************************/
 
-bool detectCycle(Node *head)
-{
-	//	Write your code here
-     /*Node* slow=head;
-
-    Node* fast=head;
-
-    while(fast!=NULL&&fast->next!=NULL){
-
-        slow=slow->next;
-
-        fast=fast->next->next;
-
-        if(slow==fast){
-
-            return true;
-
-        }
-
-    }
-
-    return false;*/
+bool detectCycle(Node *head){
     if(head == NULL) return false;
 	Node* slow = head;
 	Node* fast = head->next;
