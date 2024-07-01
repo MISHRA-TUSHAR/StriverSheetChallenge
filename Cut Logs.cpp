e86291b4f65420f2992bbf5c8c075ec754e4c84e
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2024-07-01 10:39:52 +0530

    .

--- Cut Logs.cpp
+++ Cut Logs.cpp
@@ -1,5 +1,4 @@
-int f(int k, int start, int end)
-{
+int f(int k, int start, int end){
     if(end == start)
         return 0;
     if(k == 1)
@@ -7,7 +6,6 @@ int f(int k, int start, int end)
     int mid = (start + end + 1)/2;
     return 1 + max(f(k-1, start, mid-1), f(k, mid, end));
 }
-int cutLogs(int k, int n)
-{
+int cutLogs(int k, int n){
     return f(k, 0, n);
 }
